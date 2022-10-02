66673a03a6d2e85e3479aef89ef4f94196eeef88
Brian Jalleh <brjalleh@gmail.com>
2022-10-02 14:00:50 +0100

    Test 1.1

--- primes.py
+++ primes.py
@@ -2,5 +2,24 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if(number_of_primes <= 0):
+        raise ValueError
+    
     list = []
+    
+    count = 2
+    
+    while len(list) < number_of_primes:
+        isPrime = True
+        
+        for i in range(2, count):
+            if(count%i == 0):
+                isPrime = False
+                break
+            
+        if(isPrime):
+            list.append(count)
+            
+        count += 1
+        
     return list
